231d376458f92239864ed95b5465d3884af37498
raihannaeem <raihannaeem@gmail.com>
2025-04-30 23:43:26 +0530

    transition works nice

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,9 +4,14 @@ function Home(){
     return(
         <div className="home">
             <img className="mainImg" src="/solidRaihan.png" />
-            <p className="about">
-            Hey there! I'm a second year computer science student at PES University. This is just a low-effort webpage i made to link you to a few of my project. I've worked on these in my free time and these are the projects that i've used to learn and develop my skills.
-            </p>
+            <img className="image-hover" src="/transparentRaihan.png"/>
+            <div className="about-div">
+                <h1>RAIHAN NAEEM</h1>
+                <p className="about">
+                Hey there! I'm a second year computer science student at PES University. This is just a low-effort webpage i made to link you to a few of my project. I've worked on these in my free time and these are the projects that i've used to learn and develop my skills.
+                </p>
+                <p>Kannur | Bangalore </p>
+            </div>
         </div>
     )
 }
