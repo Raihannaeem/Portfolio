797e1f95e1deff90c4d22a1e680d262303414861
raihannaeem <raihannaeem@gmail.com>
2025-05-01 16:12:04 +0530

    flight added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,6 +11,19 @@ function Home(){
                 Hey there! I'm a second year computer science student at PES University. This is just a low-effort webpage i made to link you to a few of my project. I've worked on these in my free time and these are the projects that i've used to learn and develop my skills.
                 </p>
                 <p>Kannur | Bangalore </p>
+                <svg xmlns="http://www.w3.org/2000/svg" style={{marginLeft:"0px", paddingLeft: "0px"}} width="800" height="200" viewBox="0 150 1600 100" fill="none">
+                    <g id="Frame1">
+                        <g id="lines">
+                            <path id="XMLID_886_" d="M43.2245 145.296L108.454 144.902" stroke="#263238" stroke-width="8" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path id="XMLID_885_" d="M62.7173 70.7335L181 70.4952" stroke="#263238" stroke-width="8" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path id="XMLID_884_" d="M72.4533 216.103L171.567 216.393" stroke="#263238" stroke-width="8" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <g id="flight">
+                            <path id="XMLID_144_" d="M235.23 184.806L212.977 213.271C210.667 216.226 212.366 220.538 215.843 220.565L226.943 220.658C227.827 220.66 228.711 220.371 229.454 219.817L300.247 167.869" stroke="#263238" stroke-width="8" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path id="XMLID_1011_" d="M333.205 128.807L284.613 128.43L239.928 57.5973C239.245 56.415 238.027 55.7756 236.684 55.7705L223.174 55.6618C220.487 55.6514 218.296 58.4259 218.793 61.2308L230.055 128.01L176.092 127.583C170.467 127.549 165.446 124.529 162.66 119.44L153.149 102.285C152.468 101.025 151.173 100.28 149.75 100.275L141.194 100.199C138.252 100.183 136.049 103.234 136.867 106.225L144.146 133.484C149.065 151.506 164.091 163.642 181.795 163.778L355.37 165.121C361.417 165.164 366.611 159.96 367.012 153.462C367.264 149.502 365.644 145.87 362.763 143.583L351.456 135.001C346.169 130.99 339.84 128.862 333.205 128.807Z" stroke="#263238" stroke-width="8" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                    </g>
+                </svg>
             </div>
         </div>
     )
